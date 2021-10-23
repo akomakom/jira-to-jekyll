3ee494bc0ec2788d8d9fd505c2fd5ae9258cce3a
akom <alexander.komarov@softwareag.com>
2021-10-23 19:40:39 -0400

    logger, urlencode # in filenames

--- convert.rb
+++ convert.rb
@@ -5,24 +5,28 @@ require 'netrc'
 require 'json'
 require 'erb'
 require 'fileutils'
-require 'date' 
+require 'date'
+require 'logger'
 
 @options = {
   :jql => '',
   :dir => 'jekyll',
   :max_results => 1000,
-  :debug => false,
+  :loglevel => 'WARN',
+  :debughttp => false,
   :max_overall => 1000000000,
   :attachments => false
 }
+
+
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: #{__FILE__ } [@options]"
 
-  opts.on("--url URL", "JIRA URL (base)") do |v|
+  opts.on("--url URL", "JIRA URL (base), eg https://my.domain.name") do |v|
     @options[:url] = v
   end
 
-  opts.on("--jql EXPRESSION", "Optional JQL for issue search (default is '')") do |v|
+  opts.on("--jql EXPRESSION", "Optional JQL for issue search, default is '' (all)") do |v|
     @options[:jql] = v
   end
 
@@ -30,7 +34,7 @@ parser = OptionParser.new do |opts|
     @options[:dir] = v.to_i
   end
 
-  opts.on("--attachments", "Download attachments that do not exist locally (default #{@options[:attachments]}") do |v|
+  opts.on("--attachments", "Download attachments that do not exist locally, default: #{@options[:attachments]}") do |v|
     @options[:attachments] = true
   end
 
@@ -40,15 +44,19 @@ parser = OptionParser.new do |opts|
 
   opts.on("--max-overall N", "Stop after processing this many issues overall, default: #{@options[:max_overall]}") do |v|
     @options[:max_overall] = v.to_i
+    # don't request more than max also
     @options[:max_results] = [@options[:max_results], @options[:max_overall]].min
   end
 
-  opts.on("--netrc PATH", "Path to netrc file with credentials (HOME is checked automatically).") do |v|
+  opts.on("--netrc PATH", "Path to netrc file with credentials (~/.netrc is checked automatically).") do |v|
     @options[:netrc] = v
   end
 
-  opts.on("--debug", "Turn on debug") do |v|
-    @options[:debug] = v
+  opts.on("--loglevel LEVEL", "Change log level, default: #{@options[:loglevel]}") do |v|
+    @options[:loglevel] = v
+  end
+  opts.on("--debug-http", "Turn on debug for network requests") do |v|
+    @options[:debughttp] = v
   end
 
   opts.on_tail("-h", "--help", "Show this message") do
@@ -71,23 +79,23 @@ def prompt(purpose)
   gets.chomp
 end
 
-def debug(message)
-  if @options[:debug]
-    puts "DEBUG: #{message}"
+def make_http(uri)
+  http = Net::HTTP.new(uri.host, uri.port)
+  if @options[:debughttp]
+    http.set_debug_output $stdout
   end
+  http.use_ssl = true if uri.instance_of? URI::HTTPS
+  http
 end
 
 def get_json(url)
   uri = URI(url)
   result = nil
 
-  http = Net::HTTP.new(uri.host, uri.port)
-  if @options[:debug]
-    http.set_debug_output $stdout
-  end
-  http.use_ssl = true if uri.instance_of? URI::HTTPS
+  @logger.info "GET: #{uri}"
+
+  http = make_http uri
   http.start() do |http|
-    debug "GET: #{uri}"
     request = Net::HTTP::Get.new uri
     request['Content-Type'] = 'application/json'
     request.basic_auth @options[:user], @options[:pass]
@@ -100,24 +108,18 @@ end
 
 
 def get_attachment(url, filename)
-  # hack in case of misconfigured jira, replace the url base in the JSON with what we have in options
+  # hack in case of misconfigured jira, where API calls return localhost:8080 URLs
+  # replace the url base in the JSON with what we have in options
   json_uri = URI(url)
   uri = URI(@options[:url])
   uri.path = json_uri.path
 
-  debug "Downloading attachment from #{uri} to #{filename}"
-
-  result = nil
+  @logger.info "Downloading attachment from #{uri} to #{filename}"
 
   FileUtils.mkdir_p(File.dirname(filename))
 
-  http = Net::HTTP.new(uri.host, uri.port)
-  if @options[:debug]
-    http.set_debug_output $stdout
-  end
-  http.use_ssl = true if uri.instance_of? URI::HTTPS
+  http = make_http uri
   http.start() do |http|
-    debug "GET (attachment): #{uri}"
     request = Net::HTTP::Get.new uri
     request.basic_auth @options[:user], @options[:pass]
     response = http.request request # Net::HTTPResponse object
@@ -125,7 +127,6 @@ def get_attachment(url, filename)
       file.write(response.body)
     end
   end
-  result
 
 end
 
@@ -146,21 +147,21 @@ def init_credentials
   else
     @options[:user], @options[:pass] = creds
   end
-  debug "Using credentials: #{@options[:user]}"
+  @logger.warn "Using credentials: #{@options[:user]}"
 end
 
 def process_issue(issue)
-  debug "Processing issue with content:\n#{JSON.pretty_generate(issue)}"
+  @logger.debug "Processing issue with content:\n#{JSON.pretty_generate(issue)}"
 
   begin
     output = @erb_issue.result_with_hash issue
   rescue
-    puts "Error processing issue #{issue['key']} with content:\n#{JSON.pretty_generate(issue)}"
+    @logger.fatal "Error processing issue #{issue['key']} with content:\n#{JSON.pretty_generate(issue)}"
     raise
   end
 
   filename = "#{@options[:dir_issues]}/#{issue['key']}.md"
-  debug "Writing issue file to #{filename}"
+  @logger.debug "Writing issue file to #{filename}"
   File.write filename, output
 
   if issue['fields'] && issue['fields']['project']
@@ -177,31 +178,32 @@ end
 def process_attachment(issue, attachment)
   filename = "#{@options[:dir_attachments]}/#{issue['fields']['project']['key']}/#{issue['key']}/#{attachment['filename']}"
   if File.exist? filename and File.size?(filename)
-    debug "Attachment file exists: #{filename}"
+    @logger.debug "Attachment file exists: #{filename}"
   else
     get_attachment(attachment['content'], filename)
   end
 end
 
 def process_project(project)
-  debug "Checking for project key in #{project['key']}"
   project_key = project['key']
   if project_key && !@seen_projects.include?(project_key)
     begin
       output = @erb_project.result_with_hash project
     rescue
-      puts "Error processing project #{project_key} from:\n#{JSON.pretty_generate(project)}"
+      @logger.fatal "Error processing project #{project_key} from:\n#{JSON.pretty_generate(project)}"
       raise
     end
     filename = "#{@options[:dir_projects]}/#{project_key}.md"
-    puts "Writing project file to #{filename}"
+    @logger.info "Writing project file to #{filename}"
     File.write filename, output
     @seen_projects << project_key
   end
 
 end
 
-debug "Running with options #{@options}"
+@logger = Logger.new(STDOUT)
+@logger.level = @options[:loglevel]
+@logger.info "Running with options #{@options}"
 @options[:dir_issues] = "#{@options[:dir]}/issues"
 @options[:dir_projects] = "#{@options[:dir]}/projects"
 @options[:dir_attachments] = "#{@options[:dir]}/attachments"
@@ -214,14 +216,13 @@ init_credentials
 @seen_projects = []
 
 # Step 1: count the issues
-
 result = get_json("#{@options[:url]}/rest/api/2/search?jql=#{@options[:jql]}&maxResults=1&startAt=0")
 total = result['total']
-puts "Total issue count: #{total}"
+@logger.warn "Total issue count: #{total}"
 
 index = 0
 while (index < total and index < @options[:max_overall])
-  puts "Requesting #{@options[:max_results]} issues starting at index #{index}"
+  @logger.warn "Requesting #{@options[:max_results]} issues starting at index #{index}"
   result = get_json("#{@options[:url]}/rest/api/2/search?jql=#{@options[:jql]}&maxResults=#{@options[:max_results]}&startAt=#{index}&fields=*all")
 
   result['issues'].each do |issue| process_issue(issue) end
